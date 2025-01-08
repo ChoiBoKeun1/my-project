f72a87ab9892c5e972970473c975b7be393b3e29
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-01-08 21:45:40 +0900

    data.sql 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO member (name, content) VALUES ('이름 1', '내용 1')
-INSERT INTO member (name, content) VALUES ('이름 2', '내용 2')
-INSERT INTO member (name, content) VALUES ('이름 3', '내용 3')
+INSERT INTO member (name, content) VALUES ('이름 1', '내용 1');
+INSERT INTO member (name, content) VALUES ('이름 2', '내용 2');
+INSERT INTO member (name, content) VALUES ('이름 3', '내용 3');
