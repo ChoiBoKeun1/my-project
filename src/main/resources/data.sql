ecff372e0c410cd3a376c22f2eaf673f39e1ceba
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-01-10 17:05:36 +0900

    [test] CD 잘되나 확인위해 data.sql 수정 3rd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO member (name, content) VALUES ('name 1', 'content 1');
 INSERT INTO member (name, content) VALUES ('name 2', 'content 2');
 INSERT INTO member (name, content) VALUES ('name 3', 'content 3');
-INSERT INTO member (name, content) VALUES ('name 5', 'content 5');
+INSERT INTO member (name, content) VALUES ('name 4', 'content 4');
