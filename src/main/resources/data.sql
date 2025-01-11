024d558617d047e05a0e0dedb9de01d4f5760483
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-01-11 13:53:31 +0900

    [refactor] DB 초기값 제거

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO member (name, content) VALUES ('name 1', 'content 1');
-INSERT INTO member (name, content) VALUES ('name 2', 'content 2');
-INSERT INTO member (name, content) VALUES ('name 3', 'content 3');
